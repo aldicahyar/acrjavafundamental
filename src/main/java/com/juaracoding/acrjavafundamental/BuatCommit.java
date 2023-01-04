dcc46df2498492b61d372eaf7d5b0c4474d92cd9
aldicahyar <aldicahyaramadhan1@gmail.com>
2023-01-04 20:02:51 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/acrjavafundamental/BuatCommit.java
@@ -0,0 +1,9 @@
+package com.juaracoding.acrjavafundamental;
+
+public class BuatCommit {
+
+    public static void main(String[] args) {
+        System.out.println("Buat Commit Ke Github");
+        System.out.println("Buat Commit Ke Github");
+    }
+}
